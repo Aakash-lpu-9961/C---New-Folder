a64d82c76d572a8e968acddac5431a064c804278
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-06-06 23:14:35 +0530

    Yes

---
+++ Tut1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter Number :";
+    cin>>num;
+        if (num%2==0)
+        {
+            cout<<"it is even number";
+        }
+    return 0;
+        
+}
