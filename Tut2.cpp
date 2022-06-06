f51c919877e90e24eabf9a16892028b0569eacb1
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-06-06 23:42:13 +0530

    Normal Program

---
+++ Tut2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cout<<"Enter some numbers:";
+    cin>>a>>b>>c;
+    cout<<"Entered Number is:"<<a<<b<<c;
+    return 0;
+}
